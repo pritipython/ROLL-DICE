5bb806b4de042b004b4842a92fb5d51fd182511c
Priti Gupta <79739128+pritipython@users.noreply.github.com>
2021-03-02 12:53:37 +0530

    Update ROLL_DICE.py

--- ROLL_DICE.py
+++ ROLL_DICE.py
@@ -1,8 +1,7 @@
+'''This is for rolling dice'''
 import random as r #Import random Module for Shuffling.
 i=[[6,1],[3,4],[5,2]]# i is initial Position of Dice.
 print('initial values: ',i)
-
-
 print(f"""                      back:{i[1][1]}
                 +---------------------+
                /                     /|
@@ -21,14 +20,12 @@ print(f"""                      back:{i[1][1]}
                     bottom:{i[0][1]}
                     
 ****************Let's Play******************                                       """)
-print("\nR-roll right\nL-roll left\nU-roll up\nD-roll down\nQ-quit ")
-
+print("\nR-roll right\nL-roll left\nU-roll up\nD-roll down\nQ-quit")
 r.shuffle(i)
 print(i)
-
-
-def dice():
-        res = (f"""                      back:{i[1][1]}
+def dice_final():
+    """dice() function returns the final position of dice."""
+    res = (f"""                      back:{i[1][1]}
                 +---------------------+
                /                     /|
               /        Top:{i[0][0]}        / |
@@ -43,69 +40,52 @@ def dice():
            |                     |  /
            |                     | /
            +---------------------+
-                    bottom:{i[0][1]}                             
-                                                                   """)
-        """ dice() function returns the final position of dice. """
-        while(True):
-        
-            if select=='r':
-                i[0][0],i[2][1],i[0][1],i[2][0]=i[2][0],i[0][0],i[2][1],i[0][1]
-                return res
-          
-            if select=='l':
-                i[2][0],i[0][0],i[2][1],i[0][1]=i[0][0],i[2][1],i[0][1],i[2][0]
-                return res
-
-            if select=='u':
-                i[0][0],i[1][1],i[1][0],i[0][1]=i[1][0],i[0][0],i[0][1],i[1][1]
-                return res
-
-            if select=='d':
-                i[1][0],i[0][0],i[0][1],i[1][1]=i[0][0],i[1][1],i[1][0],i[0][1]
-                return res
-
-        
-    
-
-
-def right():
-        """ This function rotates dice to the Right side """
-        i[0][0],i[2][1],i[0][1],i[2][0]=i[2][0],i[0][0],i[2][1],i[0][1]
-        return i
-
-def left():
-        """ This function rotates dice to the Left side """
-        i[2][0],i[0][0],i[2][1],i[0][1]=i[0][0],i[2][1],i[0][1],i[2][0]
-        return i
-
-def up():
-        """ This function rotates dice to the Up side """
-        i[0][0],i[1][1],i[1][0],i[0][1]=i[1][0],i[0][0],i[0][1],i[1][1]
-        return i
-
-def down():
-        """ This function rotates dice to the Down side """
-        i[1][0],i[0][0],i[0][1],i[1][1]=i[0][0],i[1][1],i[1][0],i[0][1]
-        return i
-
-
-
+                    bottom:{i[0][1]}""")
+    while True:
+        if select=='r':
+            i[0][0],i[2][1],i[0][1],i[2][0]=i[2][0],i[0][0],i[2][1],i[0][1]
+            return res
+        if select=='l':
+            i[2][0],i[0][0],i[2][1],i[0][1]=i[0][0],i[2][1],i[0][1],i[2][0]
+            return res
+        if select=='u':
+            i[0][0],i[1][1],i[1][0],i[0][1]=i[1][0],i[0][0],i[0][1],i[1][1]
+            return res
+        if select=='d':
+            i[1][0],i[0][0],i[0][1],i[1][1]=i[0][0],i[1][1],i[1][0],i[0][1]
+            return res
+def right_roll():
+    """This function rotates dice to the Right side"""
+    i[0][0],i[2][1],i[0][1],i[2][0]=i[2][0],i[0][0],i[2][1],i[0][1]
+    return i
+def left_roll():
+    """This function rotates dice to the Left side"""
+    i[2][0],i[0][0],i[2][1],i[0][1]=i[0][0],i[2][1],i[0][1],i[2][0]
+    return i
+def up_roll():
+    """This function rotates dice to the Up side"""
+    i[0][0],i[1][1],i[1][0],i[0][1]=i[1][0],i[0][0],i[0][1],i[1][1]
+    return i
+def down_roll():
+    """This function rotates dice to the Down side"""
+    i[1][0],i[0][0],i[0][1],i[1][1]=i[0][0],i[1][1],i[1][0],i[0][1]
+    return i
 while True:
-  select = ((input('pick your move: ').strip()[0]).lower())
-  if select == 'r' :
-    print(right())
-  elif select == 'l':
-    print(left())
-  elif select == 'u':
-    print(up())
-  elif select == 'd':
-    print(down())
-  elif select == 'q':
-    print("")
-    print("**********Hey!!! This is Your Final Dice**********")
-    print()
-    print(dice())
-    print("*******************Thank You*********************")
-    break
-  else:
-          print("This is Invalid move \nPlease Enter R/L/U/D to Rotate or Q to Quit. ")
+    select = ((input('pick your move: ').strip()[0]).lower())
+    if select == 'r' :
+        print(right_roll())
+    elif select == 'l':
+        print(left_roll())
+    elif select == 'u':
+        print(up_roll())
+    elif select == 'd':
+        print(down_roll())
+    elif select == 'q':
+        print("")
+        print("**********Hey!!! This is Your Final Dice**********")
+        print()
+        print(dice_final())
+        print("*******************Thank You*********************")
+        break
+    else:
+        print("This is Invalid move \nPlease Enter R/L/U/D to Rotate or Q to Quit.")
